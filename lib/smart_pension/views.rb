33bd91f48e13a120a3808a18b5f911fa42057c7b
Jamal Rjoul <46835990+jamensky@users.noreply.github.com>
2020-02-20 18:34:59 +0000

    completed logic for unique views

--- lib/smart_pension/views.rb
+++ lib/smart_pension/views.rb
@@ -3,35 +3,47 @@ require "pry"
 class Parse
 
   def initialize(file)
-   @page_and_ip = []
+   @pages_and_ip = []
+   @pages = []
+   @ip = []
+
    File.open(file, "r") do |line|
-    line.each{|x| @page_and_ip << x}
+    line.each do |x| 
+      @pages_and_ip << x 
+      @pages << x.split.first
+      @ip << x.split.last 
+    end 
    end
   end
 
-  def page_views
-    @pages = []
-    @page_and_ip.each{|x| @pages << x.split.first}
 
+  def page_views
     ranked_pages = Hash.new
     @pages.each{|x| ranked_pages[x] = @pages.count(x)}
     puts ranked_pages.sort_by{|page, views| views}.reverse
-  end
+  end 
+
 
   def unique_views
-    @ip = []
-    @page_and_ip.each{|x| @ip << x.split.last}
+    hash = {} 
+    @pages.each{|pg| hash[pg] = []}
 
-    @page_and_ip.each do|x|
-      x.split
-      @page_and_ip.each do |y|
-
-     
-     
-    binding.pry
+    @pages_and_ip.each do |x|
+      if hash.include?(x.split.first) 
+        if !hash[x.split.first].include?(x.split.last)
+          hash[x.split.first] << x.split.last
+        end
+      end 
+    end   
+    
+    ranked_pages = {}
+    hash.each do |x, y|
+      ranked_pages[x] = y.length 
+    end 
+    puts ranked_pages.sort_by{|pg, ip| ip }.reverse
   end
-
-
-
-
 end
+
+
+
+
